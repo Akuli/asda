eba4a215795a45f7206bf6979d3db72dab6ccc09
Akuli <akuviljanen17@gmail.com>
2019-01-19 20:55:13 +0200

    more tokenizer tests

--- asdac-tests/test_tokenizer.py
+++ asdac-tests/test_tokenizer.py
@@ -1,9 +1,66 @@
-from asdac import tokenizer
+import pytest
+
+from asdac.common import CompileError, Location
+from asdac.tokenizer import Token
+from asdac.tokenizer import tokenize as real_tokenize
 
 
 def tokenize(code):
-    return list(tokenizer.tokenize('test file', code))
+    return list(real_tokenize('test file', code))
+
+
+def location(startline, startcolumn, endline, endcolumn):
+    return Location('test file', startline, startcolumn, endline, endcolumn)
 
 
 def test_automatic_trailing_newline():
     assert tokenize('let x = y') == tokenize('let x = y\n')
+
+
+def test_keyword_in_id():
+    assert tokenize('func funcy ffunc func') == [
+        Token('keyword', 'func', location(1, 0, 1, 4)),
+        Token('id', 'funcy', location(1, 5, 1, 10)),
+        Token('id', 'ffunc', location(1, 11, 1, 16)),
+        Token('keyword', 'func', location(1, 17, 1, 21)),
+        Token('newline', '\n', location(1, 21, 2, 0)),
+    ]
+
+
+def test_indent():
+    assert tokenize('if x:\n  y') == [
+        Token('keyword', 'if', location(1, 0, 1, 2)),
+        Token('id', 'x', location(1, 3, 1, 4)),
+        Token('indent', '  ', location(2, 0, 2, 2)),
+        Token('id', 'y', location(2, 2, 2, 3)),
+        Token('newline', '\n', location(2, 3, 3, 0)),
+        Token('dedent', '', location(3, 0, 3, 0)),
+    ]
+
+    with pytest.raises(CompileError) as error:
+        tokenize('if x:\n     y\n z')
+    assert error.value.location == location(3, 0, 3, 1)
+    assert error.value.message == "the indentation is wrong"
+
+    assert tokenize('a:\n  b\nx:\n    y') == [
+        Token('id', 'a', location(1, 0, 1, 1)),
+        Token('indent', '  ', location(2, 0, 2, 2)),
+        Token('id', 'b', location(2, 2, 2, 3)),
+        Token('newline', '\n', location(2, 3, 3, 0)),
+        Token('dedent', '', location(3, 0, 3, 0)),
+        Token('id', 'x', location(3, 0, 3, 1)),
+        Token('indent', '    ', location(4, 0, 4, 4)),
+        Token('id', 'y', location(4, 4, 4, 5)),
+        Token('newline', '\n', location(4, 5, 5, 0)),
+        Token('dedent', '', location(5, 0, 5, 0)),
+    ]
+
+    with pytest.raises(CompileError) as error:
+        tokenize('x\n y')
+    assert error.value.location == location(2, 0, 2, 1)
+    assert error.value.message == "indent without : and newline"
+
+    with pytest.raises(CompileError) as error:
+        tokenize('x:y')
+    assert error.value.location == location(1, 1, 1, 2)
+    assert error.value.message == ": without newline and indent"
