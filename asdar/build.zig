e300922d79e4777479bc4af413cab6648a9ff9a7
Akuli <akuviljanen17@gmail.com>
2019-03-03 22:09:36 +0200

    remove old valgrind workaround
    you will need valgrind >=3.14.0 for valgrinding the code from now on

--- asdar/build.zig
+++ asdar/build.zig
@@ -8,15 +8,6 @@ pub fn build(b: *Builder) void {
 
     exe.linkSystemLibrary("c");
 
-    if (mode == Mode.Debug) {
-        // this is for valgrinding
-        //
-        // <andrewrk> Akuli, that's https://github.com/ziglang/zig/issues/896. it's
-        //            actually a valgrind bug. we have a workaround:
-        // <andrewrk> --no-rosegment compromise security to workaround valgrind bug
-        exe.setNoRoSegment(true);
-    }
-
     const run_step = b.step("run", "Run the app");
     const run_cmd = b.addCommand(".", b.env_map, [][]const u8{exe.getOutputPath()});
     run_step.dependOn(&run_cmd.step);
