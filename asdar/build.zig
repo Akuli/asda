aac901dc4fe32e8f375e0e5605c0764e99290829
Akuli <akuviljanen17@gmail.com>
2019-03-10 16:28:34 +0200

    remove unused run step, doesn't run yet in latest zig https://github.com/ziglang/zig/issues/2041
    this was never actually used anyway

--- asdar/build.zig
+++ asdar/build.zig
@@ -9,11 +9,6 @@ pub fn build(b: *Builder) void {
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("gmp");
 
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addCommand(".", b.env_map, [][]const u8{exe.getOutputPath()});
-    run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(&exe.step);
-
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 }
