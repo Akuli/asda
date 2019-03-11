463439ab04f94c207a91ab1ef4b9e3359475a8ac
Akuli <akuviljanen17@gmail.com>
2019-03-11 20:33:02 +0200

    update build script
    ziglang/zig#2041

--- asdar/build.zig
+++ asdar/build.zig
@@ -1,3 +1,9 @@
+// usage:
+//
+//  zig build -Dinclude-dir=/usr/include/x86_64-linux-gnu -Dlib-path=/usr/lib/x86_64-linux-gnu/
+//
+// replace the paths with wherever gmp is installed
+
 const Builder = @import("std").build.Builder;
 const Mode = @import("builtin").Mode;
 
@@ -5,6 +11,9 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("asdar", "main.zig");
     exe.setBuildMode(mode);
+
+    exe.addIncludeDir(b.option([]const u8, "include-dir", "E.g. /usr/include/x86_64-linux-gnu").?);
+    exe.addLibPath(b.option([]const u8, "lib-path", "E.g. /usr/include/x86_64-linux-gnu").?);
 
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("gmp");
