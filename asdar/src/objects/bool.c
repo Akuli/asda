a0499fb5a29b1dafe1cdaca241b0c1be9df14466
Akuli <akuviljanen17@gmail.com>
2019-07-21 13:38:06 +0300

    inline boolobj_c2asda and boolobj_asda2c for about 10 to 15 % speed increase

--- asdar/src/objects/bool.c
+++ asdar/src/objects/bool.c
@@ -1,23 +1,7 @@
 #include "bool.h"
-#include <assert.h>
-#include <stdbool.h>
 #include <stddef.h>
 #include "../objtyp.h"
 
 BoolObject boolobj_true = OBJECT_COMPILETIMECREATE(&boolobj_type, 0);
 BoolObject boolobj_false = OBJECT_COMPILETIMECREATE(&boolobj_type, 0);
-
-BoolObject *boolobj_c2asda(bool cbool)
-{
-	BoolObject *res = cbool ? &boolobj_true : &boolobj_false;
-	OBJECT_INCREF(res);
-	return res;
-}
-
-bool boolobj_asda2c(BoolObject *asdabool)
-{
-	assert(asdabool == &boolobj_true || asdabool == &boolobj_false);
-	return (asdabool == &boolobj_true);
-}
-
 const struct Type boolobj_type = { .methods = NULL, .nmethods = 0 };
--- asdar/src/objects/bool.h
+++ asdar/src/objects/bool.h
@@ -1,6 +1,7 @@
 #ifndef OBJECTS_BOOL_H
 #define OBJECTS_BOOL_H
 
+#include <assert.h>
 #include <stdbool.h>
 #include "../objtyp.h"
 
@@ -12,10 +13,20 @@ typedef struct BoolObject {
 
 extern BoolObject boolobj_true, boolobj_false;
 
-// never fails, always returns a new reference
-BoolObject *boolobj_c2asda(bool cbool);
+// yes, non-static inline functions are defined in header files in c
 
-// asserts that the object is boolobj_true or boolobj_false
-bool boolobj_asda2c(BoolObject *asdabool);
+// never fails, always returns a new reference
+inline BoolObject *boolobj_c2asda(bool cbool)
+{
+	BoolObject *res = cbool ? &boolobj_true : &boolobj_false;
+	OBJECT_INCREF(res);
+	return res;
+}
+
+inline bool boolobj_asda2c(BoolObject *asdabool)
+{
+	assert(asdabool == &boolobj_true || asdabool == &boolobj_false);
+	return (asdabool == &boolobj_true);
+}
 
 #endif   // OBJECTS_BOOL_H
