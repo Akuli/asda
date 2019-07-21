06bc640826097065dc07bb3c5f28bb6e62f92b8b
Akuli <akuviljanen17@gmail.com>
2019-07-21 19:34:50 +0300

    fix -O0 compiling

--- asdar/src/objects/bool.c
+++ asdar/src/objects/bool.c
@@ -5,3 +5,7 @@
 BoolObject boolobj_true = OBJECT_COMPILETIMECREATE(&boolobj_type, 0);
 BoolObject boolobj_false = OBJECT_COMPILETIMECREATE(&boolobj_type, 0);
 const struct Type boolobj_type = { .methods = NULL, .nmethods = 0 };
+
+// https://stackoverflow.com/a/18636323
+extern inline BoolObject *boolobj_c2asda(bool cbool);
+extern inline bool boolobj_asda2c(BoolObject *asdabool);
